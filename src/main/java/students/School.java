2cc994a1b08cb5cd08a7b678741382cd0d386cb6
simon <simon@dancingcloudservices.com>
2021-03-08 09:16:04 -0700

    Day One Break One

--- src/main/java/students/School.java
+++ src/main/java/students/School.java
@@ -1,15 +1,68 @@
 package students;
 
+import java.util.ArrayList;
 import java.util.List;
 
+interface CriterionOfStudent {
+  boolean test(Student s); // exactly one abstract method
+}
+
+class SmartStudent implements CriterionOfStudent {
+  private int threshold;
+
+  public SmartStudent(int threshold) {
+    this.threshold = threshold;
+  }
+
+  @Override
+  public boolean test(Student s) {
+    return s.getGrade() > threshold;
+  }
+}
+
+class EnthusiasticStudent implements CriterionOfStudent {
+  @Override
+  public boolean test(Student s) {
+    return s.getCourses().size() > 3;
+  }
+}
+
 public class School {
-  public static void showAllSmart(List<Student> students) {
+  public static void showAll(List<Student> students) {
     for (Student s : students) {
-      if (s.getGrade() > 65) {
-        System.out.println(s);
-      }
+      System.out.println(s);
     }
     System.out.println("----------------------------");
+  }
+
+//  public static List<Student> getSmart(List<Student> students, int threshold) {
+//    List<Student> rv = new ArrayList<>();
+//    for (Student s : students) {
+//      if (s.getGrade() > threshold) {
+//        rv.add(s);
+//      }
+//    }
+//    return rv;
+//  }
+//
+//  public static List<Student> getEnthusiastic(List<Student> students, int threshold) {
+//    List<Student> rv = new ArrayList<>();
+//    for (Student s : students) {
+//      if (s.getCourses().size() > threshold) {
+//        rv.add(s);
+//      }
+//    }
+//    return rv;
+//  }
+
+  public static List<Student> getByCriterion(List<Student> students, CriterionOfStudent crit) {
+    List<Student> rv = new ArrayList<>();
+    for (Student s : students) {
+      if (crit.test(s)) {
+        rv.add(s);
+      }
+    }
+    return rv;
   }
 
   public static void main(String[] args) {
@@ -18,6 +71,48 @@ public class School {
         Student.of("Jim", 58, "Art"),
         Student.of("Sheila", 89, "Math", "Physics", "Astro Physics", "Quantum Mechanics")
     );
-    showAllSmart(roster);
+    showAll(roster);
+//    showAll(getSmart(roster, 65));
+//    showAll(getEnthusiastic(roster, 2));
+    showAll(getByCriterion(roster, new SmartStudent(80)));
+    showAll(getByCriterion(roster, new EnthusiasticStudent()));
+//      showAll(getByCriterion(roster, new
+//              /*class EnthusiasticStudent implements */CriterionOfStudent() {
+//            @Override
+//            public boolean test(Student s) {
+//              return s.getCourses().size() > 3;
+//            }
+//          }
+//      ));
+//      showAll(getByCriterion(roster, /*new
+//              CriterionOfStudent() {
+//            @Override
+//            public boolean test*/(Student s) -> {
+//              return s.getCourses().size() > 3;
+//            }
+//          /*}*/
+//      ));
+
+//    showAll(getByCriterion(roster, (Student s) -> {
+//      return s.getCourses().size() < 3;
+//    }));
+//
+    showAll(getByCriterion(roster, (s) -> {
+      return s.getCourses().size() < 3;
+    }));
+
+    CriterionOfStudent midRange = (Student s) -> {
+      return s.getGrade() < 80 && s.getGrade() > 70;
+    };
+
+    /// NO NO NO! Must be an interface context, and
+    // interface must define EXACTLY ONE abstract method
+    // And, that must be the ONLY method we wish to implement.
+//    Object midRange = (Student s) -> {
+//      return s.getGrade() < 80 && s.getGrade() > 70;
+//    };
+
+    showAll(getByCriterion(roster, midRange));
   }
 }
+
